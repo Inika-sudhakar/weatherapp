105111b897f1ef1c1d44b68bef5b12194e9e17a2
Inika-sudhakar <inikasudhakar@gmail.com>
2024-08-01 15:14:08 +0530

    first commit

---
+++ Weather-App-main/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './components/currentLocation';
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Weather />
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
